8086395c78436970d3e1bd80a746e159102916e1
Alessandro Tundo <alessandrotundo94@gmail.com>
2017-04-04 18:11:37 +0200

    Added comments to Author model and removed useless imports
    Signed-off-by: Alessandro Tundo <alessandrotundo94@gmail.com>

--- src/main/java/org/wikitolearn/wikirating/model/Author.java
+++ src/main/java/org/wikitolearn/wikirating/model/Author.java
@@ -4,9 +4,6 @@ import org.neo4j.ogm.annotation.EndNode;
 import org.neo4j.ogm.annotation.GraphId;
 import org.neo4j.ogm.annotation.RelationshipEntity;
 import org.neo4j.ogm.annotation.StartNode;
-import org.neo4j.ogm.annotation.typeconversion.DateLong;
-
-import java.util.Date;
 
 /**
  * This entity represent the edit of a user on a Page.
@@ -22,45 +19,80 @@ public class Author {
     private User user;
     @EndNode
     private Revision revision;
+	/**
+	 * 
+	 */
+	public Author() {}
+	
+	/**
+	 * @param reliability
+	 */
+	public Author(double reliability) {
+		this.reliability = reliability;
+	}
 
-    public Author() {
-    }
+	/**
+	 * @return the graphId
+	 */
+	public Long getGraphId() {
+		return graphId;
+	}
 
-    public Author(double reliability){
-        this.reliability = reliability;
-    }
+	/**
+	 * @param graphId the graphId to set
+	 */
+	public void setGraphId(Long graphId) {
+		this.graphId = graphId;
+	}
 
-    public double getReliability() {
-        return reliability;
-    }
+	/**
+	 * @return the reliability
+	 */
+	public double getReliability() {
+		return reliability;
+	}
 
-    public void setReliability(double reliability) {
-        this.reliability = reliability;
-    }
+	/**
+	 * @param reliability the reliability to set
+	 */
+	public void setReliability(double reliability) {
+		this.reliability = reliability;
+	}
 
-    public User getUser() {
-        return user;
-    }
+	/**
+	 * @return the user
+	 */
+	public User getUser() {
+		return user;
+	}
 
-    public void setUser(User user) {
-        this.user = user;
-    }
+	/**
+	 * @param user the user to set
+	 */
+	public void setUser(User user) {
+		this.user = user;
+	}
 
-    public Revision getRevision() {
-        return revision;
-    }
+	/**
+	 * @return the revision
+	 */
+	public Revision getRevision() {
+		return revision;
+	}
 
-    public void setRevision(Revision revision) {
-        this.revision = revision;
-    }
+	/**
+	 * @param revision the revision to set
+	 */
+	public void setRevision(Revision revision) {
+		this.revision = revision;
+	}
 
-    @Override
-    public String toString() {
-        return "Author{" +
-                "graphId=" + graphId +
-                ", reliability=" + reliability +
-                ", user=" + user +
-                ", revision=" + revision +
-                '}';
-    }
+	/* (non-Javadoc)
+	 * @see java.lang.Object#toString()
+	 */
+	@Override
+	public String toString() {
+		return "Author [graphId=" + graphId + ", reliability=" + reliability + ", user=" + user + ", revision="
+				+ revision + "]";
+	}
 }
