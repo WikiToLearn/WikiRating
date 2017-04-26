1bee6b7f446709e19cb700b74a346154a49abd3d
Alessandro Tundo <alessandrotundo94@gmail.com>
2017-04-26 18:21:55 +0200

    Implemented boilerplate method of page controller to get last revision by page id
    Added lang path variable to the PageController
    
    Signed-off-by: Alessandro Tundo <alessandrotundo94@gmail.com>

--- src/main/java/org/wikitolearn/wikirating/controller/PageController.java
+++ src/main/java/org/wikitolearn/wikirating/controller/PageController.java
@@ -8,26 +8,33 @@ import java.util.List;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.wikitolearn.wikirating.exception.GenericException;
+import org.wikitolearn.wikirating.exception.PageNotFoundException;
+import org.wikitolearn.wikirating.model.Page;
 import org.wikitolearn.wikirating.model.Revision;
 import org.wikitolearn.wikirating.model.Vote;
+import org.wikitolearn.wikirating.service.PageService;
 
 /**
  * @author aletundo RESTController for pages resources. It handles all the
  *         requests to /pages.
  */
 @RestController
-@RequestMapping("/pages/")
+@RequestMapping("{lang}/pages/")
 public class PageController {
 	private static final Logger LOG = LoggerFactory.getLogger(PageController.class);
+	@Autowired
+	private PageService pageService;
 	
 	/**
-	 * Handle GET requests on /pages/{pageId} URI. The last revision of the page is returned.
+	 * Handle GET requests on {lang}/pages/{pageId} URI. The last revision of the page is returned.
 	 * 
 	 * @param pageId
 	 *            int The id of the page
@@ -35,13 +42,18 @@ public class PageController {
 	 */
 	@RequestMapping(value = "{pageId}", method = RequestMethod.GET, produces = "application/json")
 	@ResponseBody
-	public Revision getLastRevisionByPageId(@PathVariable("pageId") int pageId) {
-		// TODO Get the last available revision for the requested page
-		return new Revision();
+	public Revision getLastRevisionByPageId(@PathVariable("lang") String lang, @PathVariable("pageId") int pageId) {
+		try{
+			Page page = pageService.getPage(pageId, lang);
+			return page.getLastRevision();
+		}catch(PageNotFoundException e){
+			LOG.error("Impossible to get the last revision of {}_{}: {}",lang, pageId, e.getMessage());
+			throw new GenericException(e.getMessage());
+		}
 	}
 	
 	/**
-	 * Handle GET requests on /pages/{pageId}/revisions URI. All the revision for the requested page are returned.
+	 * Handle GET requests on {lang}/pages/{pageId}/revisions URI. All the revision for the requested page are returned.
 	 * 
 	 * @param pageId
 	 *            int The id of the page
@@ -49,13 +61,13 @@ public class PageController {
 	 */
 	@RequestMapping(value = "{pageId}/revisions", method = RequestMethod.GET, produces = "application/json")
 	@ResponseBody
-	public List<Revision> getAllPageRevisions(@PathVariable("pageId") int pageId) {
+	public List<Revision> getAllPageRevisions(@PathVariable("lang") String lang, @PathVariable("pageId") int pageId) {
 		// TODO Get the requested revision
 		return new ArrayList<Revision>();
 	}
 
 	/**
-	 * Handle GET requests on /pages/{pageId}/revisions/{revId} URI. The requested page revision is returned.
+	 * Handle GET requests on {lang}/pages/{pageId}/revisions/{revId} URI. The requested page revision is returned.
 	 * 
 	 * @param pageId
 	 *            int The id of the page
@@ -65,13 +77,13 @@ public class PageController {
 	 */
 	@RequestMapping(value = "{pageId}/revisions/{revId}", method = RequestMethod.GET, produces = "application/json")
 	@ResponseBody
-	public Revision getRevisionById(@PathVariable("pageId") int pageId, @PathVariable("revId") int revId) {
+	public Revision getRevisionById(@PathVariable("lang") String lang, @PathVariable("pageId") int pageId, @PathVariable("revId") int revId) {
 		// TODO Get the requested revision
 		return new Revision();
 	}
 	
 	/**
-	 * Handle GET requests on /pages/{pageId}/revisions/{revId}/votes URI. All
+	 * Handle GET requests on {lang}/pages/{pageId}/revisions/{revId}/votes URI. All
 	 * the votes for the requested page revision are returned.
 	 * 
 	 * @param pageId
@@ -82,14 +94,14 @@ public class PageController {
 	 */
 	@RequestMapping(value = "{pageId}/revisions/{revId}/votes", method = RequestMethod.GET, produces = "application/json")
 	@ResponseBody
-	public List<Vote> showVotes(@PathVariable("pageId") int pageId, @PathVariable("revId") int revId) {
+	public List<Vote> showVotes(@PathVariable("lang") String lang, @PathVariable("pageId") int pageId, @PathVariable("revId") int revId) {
 
 		// TODO Get votes from PageService/PageDAO
 		return new ArrayList<Vote>();
 	}
 
 	/**
-	 * Handle POST requests on /pages/{pageId}/revisions/{revId}/votes URI. It
+	 * Handle POST requests on {lang}/pages/{pageId}/revisions/{revId}/votes URI. It
 	 * expects vote and userId as request parameters.
 	 * 
 	 * @param pageId
@@ -104,7 +116,7 @@ public class PageController {
 	 */
 	@RequestMapping(value = "{pageId}/revisions/{revId}/votes", method = RequestMethod.POST, produces = "application/json")
 	@ResponseBody
-	public Vote addVote(@PathVariable("pageId") int pageId, @PathVariable("revId") int revId,
+	public Vote addVote(@PathVariable("lang") String lang, @PathVariable("pageId") int pageId, @PathVariable("revId") int revId,
 			@RequestParam("vote") double vote, @RequestParam("userId") int userId) {
 		// TODO Insert the vote into the graph
 		return new Vote();
